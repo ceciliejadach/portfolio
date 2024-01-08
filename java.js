c8721660b620c67d9732ae2c3244d9408c21a01d
Cecilie <cepe0001@stud.kea.dk>
2024-01-08 15:22:02 +0100

    tilføjet css scroll animation

--- java.js
+++ java.js
@@ -1,7 +1,6 @@
 const menu_btn = document.querySelector(".hamburger");
 const mobile_menu = document.querySelector(".mobile-nav");
 
-var reveals = document.querySelectorAll(".reveal");
 var image = document.getElementById("billede");
 
 window.addEventListener("load", sidenVises);
@@ -13,24 +12,6 @@ function sidenVises() {
     menu_btn.classList.toggle("is-active");
     mobile_menu.classList.toggle("is-active");
   });
-}
-
-window.addEventListener("scroll", reveal);
-
-function reveal() {
-  console.log("reveal");
-
-  for (var i = 0; i < reveals.length; i++) {
-    var windowheight = window.innerHeight;
-    var revealtop = reveals[i].getBoundingClientRect().top;
-    var revealpoint = 150;
-
-    if (revealtop < windowheight - revealpoint) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
-    }
-  }
 }
 
 function moveImage(event) {
